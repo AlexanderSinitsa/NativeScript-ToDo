82d52dbc1699b60822733f16e57a375f00e58290
Alex <alexander.sinitsa@valor-software.com>
2019-04-26 15:55:26 +0300

    Added the side-menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
+import { Page } from 'tns-core-modules/ui/page';
 
 import * as fromRoot from '~/app/store/reducers';
 import { Task } from '~/app/models/task';
@@ -13,16 +14,37 @@ import * as taskAction from '~/app/store/actions/tasks';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     tasks$: Observable<Task[]>;
+    @ViewChild("rsd") rSideDrawer: ElementRef;
 
-    constructor(private store: Store<fromRoot.State>) {
+    pages: { index: number, name: string }[] = [
+        {index: 0, name: "Home"},
+        {index: 1, name: "Done tasks"},
+        {index: 2, name: "Settings"}
+    ];
+
+    constructor(private store: Store<fromRoot.State>, private page: Page) {
         // this.tasks$ = store.select(fromRoot.getAllTasks);
         this.tasks$ = store.pipe(select(fromRoot.getAllTasks));
     }
 
     deleteDoneTasks() {
         this.store.dispatch(new taskAction.DeleteDoneTasks())
+    }
+
+
+    ngOnInit(): void {
+        this.page.actionBarHidden = true;
+    }
+
+
+    onOpenDrawerTap(args): void {
+        this.rSideDrawer.nativeElement.toggleDrawerState();
+    }
+
+    goToPage(args): void {
+        this.rSideDrawer.nativeElement.toggleDrawerState();
     }
 
 
